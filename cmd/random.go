d33571d5d6a5d6b160ad6489934c09c9b5aa136f
Raymond <koolrays2000@gmail.com>
2023-09-13 19:46:43 -0600

    First commit for Dadjoke

---
+++ cmd/random.go
@@ -0,0 +1,139 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "Get a random dad joke",
+	Long:  `This command fetches a random dad joke from the icanhazdadjoke api.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		jokeTerm, _ := cmd.Flags().GetString("term")
+
+		if jokeTerm != "" {
+			getRandomJokeWithTerm(jokeTerm)
+		} else {
+			getRandomJoke()
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+	randomCmd.PersistentFlags().String("term", "", "A search term for a dad joke.")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// randomCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// randomCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+type Joke struct {
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
+}
+
+type SearchResult struct {
+	Results    json.RawMessage `json:"results"`
+	SearchTerm string          `json:"search_term"`
+	Status     int             `json:"status"`
+	TotalJokes int             `json:"total_jokes"`
+}
+
+func getRandomJoke() {
+	url := "https://icanhazdadjoke.com/"
+	responseBytes := getJokeData(url)
+	joke := Joke{}
+
+	if err := json.Unmarshal(responseBytes, &joke); err != nil {
+		fmt.Printf("Could not unmarshal responseBytes. %v", err)
+	}
+
+	fmt.Printf(string(joke.Joke))
+}
+
+func getRandomJokeWithTerm(jokeTerm string) {
+	total, results := getJokeDataWithTerm(jokeTerm)
+	randomiseJokeList(total, results)
+}
+
+func getJokeData(baseAPIUrl string) []byte {
+	request, err := http.NewRequest(
+		http.MethodGet, //method
+		baseAPIUrl,     //url
+		nil,            //body
+	)
+
+	if err != nil {
+		log.Printf("Could not request a dadjoke. %v", err)
+	}
+
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("User-Agent", "Dadjoke CLI (https://github.com/koolrays/dadjoke)")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Printf("Could not make the request. %v", err)
+	}
+	responseBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Could not read response body. %v", err)
+	}
+
+	return responseBytes
+}
+
+func getJokeDataWithTerm(jokeTerm string) (totalJokes int, jokeList []Joke) {
+	url := fmt.Sprintf("https://icanhazdadjoke.com/search?term=%s", jokeTerm)
+	responseBytes := getJokeData(url)
+
+	jokeListRaw := SearchResult{}
+
+	if err := json.Unmarshal(responseBytes, &jokeListRaw); err != nil {
+		log.Printf("Could not unmarshal responseBytes. %v", err)
+	}
+
+	jokes := []Joke{}
+	if err := json.Unmarshal(jokeListRaw.Results, &jokes); err != nil {
+		log.Printf("Could not unmarshal reponseBytes. %v", err)
+	}
+
+	return jokeListRaw.TotalJokes, jokes
+}
+func randomiseJokeList(length int, jokeList []Joke) {
+	rand.New(rand.NewSource(time.Now().Unix()))
+	min := 0
+	max := length - 1
+
+	if length <= 0 {
+		err := fmt.Errorf("No jokes found with this term")
+		fmt.Println(err.Error())
+	} else if length == 1 {
+		fmt.Println(jokeList[length-1].Joke)
+
+	} else {
+		randomNum := min + rand.Intn(max-min)
+		fmt.Println(jokeList[randomNum].Joke)
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/koolrays/dadjoke/cmd"
+
+func main() {
+	cmd.Execute()
+}
